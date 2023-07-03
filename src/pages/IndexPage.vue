e28e0d25456dfd745baff65fa5aeed43036f78c9
HeYaoDaDa <3050166231@qq.com>
2023-07-03 23:06:54 +0800

    move auth code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -12,20 +12,8 @@
 import { QPullToRefresh } from 'quasar';
 import ModItem from 'src/components/ModItem.vue';
 import { useMainDataStore } from 'src/stores/MainData';
-import { useAuthDataStore } from 'src/stores/AuthData';
-import { useQuasar } from 'quasar';
 
-const quasar = useQuasar();
 const mainDataStore = useMainDataStore();
-const authDataStore = useAuthDataStore();
-
-if (!authDataStore.activeToken) {
-  quasar.loading.show({ message: '跳转Github授权页中...' });
-  window.open(
-    'https://github.com/login/oauth/authorize?client_id=Iv1.23bebc2931676eb7',
-    '_self'
-  );
-}
 
 function refresh(done: () => void) {
   mainDataStore.updateData().finally(done);
