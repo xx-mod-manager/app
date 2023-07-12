f86f72858225fa3111743b8a836cd5dad54a63b8
HeYaoDaDa <3050166231@qq.com>
2023-07-12 23:40:19 +0800

    add search

--- src/pages/ModsPage.vue
+++ src/pages/ModsPage.vue
@@ -4,8 +4,19 @@
       class="fit row wrap justify-start items-start content-start"
       style="padding-left: 0.3rem; padding-right: 0.3rem"
     >
+      <q-input
+        v-model="searchText"
+        rounded
+        outlined
+        class="col-12"
+        style="margin-top: 0.4rem"
+      >
+        <template #append>
+          <q-avatar :icon="matSearch" />
+        </template>
+      </q-input>
       <ModItem
-        v-for="mod in mainDataStore.mods"
+        v-for="mod in result"
         :key="mod.id"
         class="col-12"
         style="margin-top: 0.4rem"
@@ -20,11 +31,24 @@ import { QPullToRefresh, useQuasar } from 'quasar';
 import { myLogger } from 'src/boot/logger';
 import ModItem from 'src/components/ModItem.vue';
 import { useMainDataStore } from 'src/stores/MainData';
-import { onMounted, ref } from 'vue';
-import { matRefresh } from '@quasar/extras/material-icons';
+import { computed, onMounted, ref } from 'vue';
+import { matRefresh, matSearch } from '@quasar/extras/material-icons';
+import Fuse from 'fuse.js';
 
 const mainDataStore = useMainDataStore();
 const { loading } = useQuasar();
+const searchText = ref('');
+const pullRefresh = ref(null as QPullToRefresh | null);
+
+const fuse = new Fuse(mainDataStore.mods, {
+  keys: ['name', 'description'],
+});
+
+const result = computed(() =>
+  searchText.value.length > 0
+    ? fuse.search(searchText.value).map((it) => it.item)
+    : mainDataStore.mods
+);
 
 function refresh(done: () => void) {
   myLogger.debug('refresh main data');
@@ -33,8 +57,6 @@ function refresh(done: () => void) {
     done();
   });
 }
-
-const pullRefresh = ref(null as QPullToRefresh | null);
 
 onMounted(() => {
   if (
