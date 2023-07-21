4de0e4302a2f4ee2135ed144a7c5869c0e134766
HeYaoDaDa <3050166231@qq.com>
2023-07-21 15:49:43 +0800

    update LeftDrawer

--- src/components/LeftDrawer.vue
+++ src/components/LeftDrawer.vue
@@ -1,44 +1,63 @@
 <template>
-  <div
-    style="padding: 0.5rem"
-    class="fit row wrap justify-start items-center content-start"
-  >
-    <q-avatar class="">
-      <img :src="mainDataStore.user.avatarUrl" />
-    </q-avatar>
+  <div>
+    <div id="userInfoDiv" class="row justify-start items-center content-start">
+      <q-avatar>
+        <img :src="mainDataStore.user.avatarUrl" />
+      </q-avatar>
 
-    <span class="text-subtitle2" style="padding-left: 1rem">{{
-      mainDataStore.user.login
-    }}</span>
+      <span class="text-subtitle1" style="padding-left: 1rem">{{
+        mainDataStore.user.login
+      }}</span>
 
-    <q-space />
+      <q-space />
 
-    <q-btn flat round :icon="matLogout" @click="logout" />
+      <q-btn flat round :icon="matLogout" @click="logout" />
+    </div>
 
-    <q-list class="col-12" style="margin-top: 1rem">
-      <q-item clickable @click="goHome">
-        <q-item-section class="text-center">首页</q-item-section>
-      </q-item>
-    </q-list>
+    <q-tabs inline-label vertical>
+      <q-route-tab
+        name="asset"
+        :icon="matDownload"
+        label="首页"
+        :to="{ name: 'assets' }"
+      />
+      <q-route-tab
+        v-if="quasar.platform.is.electron"
+        name="assetManager"
+        :icon="matWidgets"
+        label="资源管理"
+        :to="{ name: 'assetManager' }"
+      />
+    </q-tabs>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useMainDataStore } from 'src/stores/MainData';
-import { matLogout } from '@quasar/extras/material-icons';
+import {
+  matLogout,
+  matDownload,
+  matWidgets,
+} from '@quasar/extras/material-icons';
 import { useAuthDataStore } from 'src/stores/AuthData';
 import { useRouter } from 'vue-router';
+import { useQuasar } from 'quasar';
 
 const mainDataStore = useMainDataStore();
 const authDataStore = useAuthDataStore();
 const router = useRouter();
+const quasar = useQuasar();
 
 function logout() {
   authDataStore.clearAuthInfo();
   router.push({ name: 'login' });
 }
-
-function goHome() {
-  router.push({ name: 'home' });
-}
 </script>
+
+<style lang="scss">
+div #userInfoDiv {
+  padding: 0.5rem;
+  background-color: $primary;
+  color: white;
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="hHr LpR fFf">
-    <q-header elevated>
+    <q-header>
       <q-toolbar>
         <q-btn
           v-if="quasar.platform.is.mobile"
@@ -12,32 +12,12 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <q-tabs
-          v-if="quasar.platform.is.electron"
-          inline-label
-          class="bg-primary text-white"
-        >
-          <q-route-tab
-            name="asset"
-            :icon="matDownload"
-            label="资源"
-            :to="{ name: 'assets' }"
-          />
-          <q-route-tab
-            v-if="quasar.platform.is.electron"
-            name="assetManager"
-            :icon="matWidgets"
-            label="管理资源"
-            :to="{ name: 'assetManager' }"
-          />
-        </q-tabs>
+        <q-toolbar-title> Github Asset Community </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <template v-if="authDataStore.activeToken">
-      <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-drawer v-model="leftDrawerOpen">
         <LeftDrawer />
       </q-drawer>
 
@@ -51,16 +31,12 @@
 <script setup lang="ts">
 import { useAuthDataStore } from 'src/stores/AuthData';
 import { ref } from 'vue';
-import {
-  matTune,
-  matDownload,
-  matWidgets,
-} from '@quasar/extras/material-icons';
+import { matTune } from '@quasar/extras/material-icons';
 import LeftDrawer from 'src/components/LeftDrawer.vue';
 import { useQuasar } from 'quasar';
 
 const quasar = useQuasar();
 const authDataStore = useAuthDataStore();
 
-const leftDrawerOpen = ref(false);
+const leftDrawerOpen = ref(!quasar.platform.is.mobile);
 </script>
