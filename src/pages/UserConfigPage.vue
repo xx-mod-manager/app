fb6472b5309ad9c9478965f70c30bdd2c19470ef
HeYaoDaDa <3050166231@qq.com>
2023-08-14 21:42:31 +0800

    add Game method

--- src/pages/UserConfigPage.vue
+++ src/pages/UserConfigPage.vue
@@ -60,20 +60,82 @@
         </q-item-section>
       </q-item>
     </q-list>
+
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn
+        fab
+        :icon="matAdd"
+        color="primary"
+        label="添加新游戏"
+        @click="openAddNewGame"
+      />
+    </q-page-sticky>
+
+    <q-dialog v-model="addNewGameDialog">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">添加新游戏</div>
+          <q-input
+            v-model="newGameId"
+            label="游戏id"
+            :rules="[
+              (value) => validateNewGameId(value) || '请输入有效且不重复的值.',
+            ]"
+          />
+
+          <q-input
+            v-model="newGameName"
+            label="游戏名称"
+            :rules="[
+              (value) =>
+                validateNewGameName(value) || '请输入有效且不重复的值.',
+            ]"
+          />
+
+          <div @click="selectNewGameInstallPath">
+            <q-field
+              v-model="newGameInstallPath"
+              class="cursor-pointer"
+              borderless
+              stack-label
+              label="mod安装路径"
+            >
+              <template #control="props">
+                <div>
+                  {{ props.modelValue }}
+                </div>
+              </template>
+            </q-field>
+          </div>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn v-close-popup flat label="取消" color="primary" />
+          <q-btn flat label="确定" color="primary" @click="addNewGame" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { matAdd } from '@quasar/extras/material-icons';
 import { QSelect, useQuasar } from 'quasar';
 import { myLogger } from 'src/boot/logger';
+import { Game } from 'src/class/Game';
 import { useMainDataStore } from 'src/stores/MainData';
+import { useTempDataStore } from 'src/stores/TempData';
 import { useUserConfigStore } from 'src/stores/UserConfig';
 import { openDialogSelectDirectory } from 'src/utils/DialogUtils';
 import { ref } from 'vue';
 
 const userConfigStore = useUserConfigStore();
 const mainDataStore = useMainDataStore();
-const { platform } = useQuasar();
+const { platform, notify } = useQuasar();
+const addNewGameDialog = ref(false);
+const newGameId = ref(undefined as string | undefined);
+const newGameName = ref(undefined as string | undefined);
+const newGameInstallPath = ref(undefined as string | undefined);
 
 async function selectSteamAppsPath() {
   const dir = await openDialogSelectDirectory('请选择mod安装路径');
@@ -97,6 +159,69 @@ async function selectGameInstallPath() {
   } else {
     myLogger.debug('Files is empty');
   }
+}
+
+function validateNewGameId(value: undefined | string): boolean {
+  myLogger.debug('start validateNewGameId');
+  if (value == null || value.length === 0) {
+    return false;
+  }
+  if (mainDataStore.games.has(value)) {
+    return false;
+  }
+  return true;
+}
+
+function validateNewGameName(value: undefined | string): boolean {
+  if (value == null || value.length === 0) {
+    return false;
+  }
+  if (
+    Array.from(mainDataStore.games.values()).find((it) => it.name === value) !=
+    null
+  ) {
+    return false;
+  }
+  return true;
+}
+
+async function selectNewGameInstallPath() {
+  const dir = await openDialogSelectDirectory('请选择mod安装路径');
+
+  if (dir != null) {
+    newGameInstallPath.value = dir.path;
+  } else {
+    myLogger.debug('Files is empty');
+  }
+}
+
+function openAddNewGame() {
+  addNewGameDialog.value = true;
+  newGameId.value = undefined;
+  newGameName.value = undefined;
+  newGameInstallPath.value = undefined;
+}
+
+function addNewGame() {
+  //TODO: validate and add userConfig refactor.
+  if (
+    newGameId.value == null ||
+    newGameName.value == null ||
+    newGameInstallPath.value == null
+  ) {
+    notify({
+      type: 'warning',
+      message: '请填写所有必填字段!',
+    });
+    return;
+  }
+  const newGame = new Game({ id: newGameId.value, name: newGameName.value });
+  mainDataStore.games.set(newGame.id, newGame);
+  userConfigStore.updateGames([newGame]);
+  useTempDataStore().initLocalGames([newGame]);
+  userConfigStore.gameConfigs.get(newGame.id)!.installPath =
+    newGameInstallPath.value;
+  addNewGameDialog.value = false;
 }
 
 const gameSelect = ref(null as QSelect | null);
